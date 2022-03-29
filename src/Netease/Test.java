3e0db11e6205e0b562954709e198c64b2401d62f
Alessio <1013797789@qq.com>
2022-03-29 20:03:55 +0800

    一些学习进度

---
+++ src/Netease/Test.java
@@ -0,0 +1,11 @@
+package Netease;
+
+/**
+ * @auther Alessio
+ * @date 2022/3/27
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
